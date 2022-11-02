9ce09df812896840e24e730ad414c590de9de1fb
Stepan Farlom <stepan.farlom@gmail.com>
2022-11-02 22:42:31 +0300

    feature2

--- calc/main.cpp
+++ calc/main.cpp
@@ -7,42 +7,50 @@ int main()
 {
     float x1, x2; // x1 и x2 это слагаемые\множители
     char symbol; // символ, отвечающий за операцию
-    bool bl = true;
-    cin >> x1 >> symbol >> x2;
 
 
 
+    char choice;
 
-    //feature сделать проверку символа
-    //feature бесконечный калькулятор
-    if (symbol != '+' and symbol != '-' and symbol != '/' and symbol != '*'){
-        do {
-            cout << "this symbol is not ok, write new symbol: ";
-            cin >> x1 >> symbol >> x2;
 
-        } while ((symbol != '+' and symbol != '-' and symbol != '/' and symbol != '*'));
+    do {
+        bool bl = true;
 
-    }
+        cin >> x1 >> symbol >> x2;
 
-    bl = true;
-    switch(symbol)
-    {
-    case '+':
-        cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Sum(x1, x2);
-        break;
+        if (symbol != '+' and symbol != '-' and symbol != '/' and symbol != '*'){
+            do {
+                cout << "this symbol is not ok, write new symbol: ";
+                cin >> x1 >> symbol >> x2;
 
-    case '-':
-        cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Substract(x1, x2);
-        break;
+            } while ((symbol != '+' and symbol != '-' and symbol != '/' and symbol != '*'));
 
-    case '/':
-        cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Division(x1, x2);
-        break;
+        }
 
-    case '*':
-        cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Multiply(x1, x2);
-        break;
+        bl = true;
+        switch(symbol)
+        {
+        case '+':
+            cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Sum(x1, x2);
+            break;
 
-    }
+        case '-':
+            cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Substract(x1, x2);
+            break;
+
+        case '/':
+            cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Division(x1, x2);
+            break;
+
+        case '*':
+            cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Multiply(x1, x2);
+            break;
+
+        }
+        cout << "\nIf you want to continue enter Y: ";
+        cin >> choice;
+
+    } while (choice == 'Y' or choice == 'y');
+
 }
 
