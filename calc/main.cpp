0ce34762619e326ca41480fbb63d3fbc4a966de4
ilyaDevops <ilya.gasan2020@mail.ru>
2022-11-02 22:20:43 +0300

    feature1

--- calc/main.cpp
+++ calc/main.cpp
@@ -7,12 +7,24 @@ int main()
 {
     float x1, x2; // x1 и x2 это слагаемые\множители
     char symbol; // символ, отвечающий за операцию
-
+    bool bl = true;
     cin >> x1 >> symbol >> x2;
+
+
+
 
     //feature сделать проверку символа
     //feature бесконечный калькулятор
+    if (symbol != '+' and symbol != '-' and symbol != '/' and symbol != '*'){
+        do {
+            cout << (symbol != '+' and symbol != '-' and symbol != '/' and symbol != '*') << "this symbol is not ok, write new symbol: ";
+            cin >> x1 >> symbol >> x2;
 
+        } while (!(symbol != '+' or symbol != '-' or symbol != '/' or symbol != '*'));
+
+    }
+
+    bl = true;
     switch(symbol)
     {
     case '+':
@@ -30,6 +42,7 @@ int main()
     case '*':
         cout << x1 << ' ' << symbol << ' ' << x2 << " = " << Multiply(x1, x2);
         break;
+
     }
 }
 
